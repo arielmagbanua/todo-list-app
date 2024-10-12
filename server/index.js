63fdb79d1b341fe825922f83f7227d78bc10c1c3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-10-12 11:44:40 +0800

    Defined the routes.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 
+import todosRouter, { something, somehow } from "./routes/todos.js";
+
 const app = express();
 
 const port = 4000;
 
+console.log(something);
+console.log(somehow);
+
 app.get("/", (req, res) => {
   res.send("Hello Todo App!!!");
 });
+
+app.use("/api", todosRouter);
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
---
+++ server/routes/todos.js
@@ -0,0 +1,40 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/todos", (req, res) => {
+  res.send("All Todos");
+});
+
+router.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Getting TODO with id ${id}`);
+});
+
+router.post("/todos", (req, res) => {
+  res.send({ id: 1, title: "Todo", description: "My todo" });
+});
+
+router.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Updating TODO with id ${id}`);
+});
+
+router.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Deleting TODO with id ${id}`);
+});
+
+export const something = {
+  name: "something",
+  age: 20,
+};
+
+export const somehow = {
+  name: "somehow",
+  age: 20,
+};
+
+export default router;
